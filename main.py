4c4b3825d3bf019285d95d27f2f15df8723e0c1f
GripiotisMike <gripiotism@yahoo.com>
2024-11-16 16:08:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+
+# ASCII Art for Rock, Paper, Scissors
 rock = '''
     _______
 ---'   ____)
@@ -25,57 +28,34 @@ scissors = '''
 ---.__(___)
 '''
 
-import random
+# List of weapons for easy management
+weapons = [rock, paper, scissors]
 
-while True:
-    print("Lets play Rock, Paper,  Scissors! Choose your weapon!")
-    x = int(input("Type 0 for Rock, 1 for Paper or 2 for Scissors."))
-    c = random.randint(0, 2)
-    if x != 0 and x != 1 and x != 2:
-        print("Thanks for playing! Goodbye!")
-        break
-    if x == 0:
-        print("You picked Rock!")
-        print(rock)
-        if c == 0:
-            print("And I picked Rock!")
-            print(rock)
-            print("Its a tie!\n \n \n")
-        elif c == 1:
-            print("And I picked Paper!")
-            print(paper)
-            print("You lose!\n \n \n")
-        else:
-            print("And I picked Scissors!")
-            print(scissors)
-            print("You win!\n \n \n")
-    elif x == 1:
-        print("You picked Paper!")
-        print(paper)
-        if c == 0:
-            print("And I picked Rock!")
-            print(rock)
-            print("You win!\n \n \n")
-        elif c == 1:
-            print("And I picked paper!")
-            print(paper)
-            print("Its a tie!\n \n \n")
-        else:
-            print("And I picked Scissors!")
-            print(scissors)
-            print("You lose!\n \n \n")
-    else:
-        print("You picked Scissors!")
-        print(scissors)
-        if c == 0:
-            print("And I picked Rock!")
-            print(rock)
-            print("You lose!\n \n \n")
-        elif c == 1:
-            print("And I picked paper!")
-            print(paper)
-            print("You win!\n \n \n")
-        else:
-            print("And I picked Scissors!")
-            print(scissors)
-            print("Its a tie!\n \n \n")
+def play_game():
+    print("Let's play Rock, Paper, Scissors! Choose your weapon!")
+    while True:
+        try:
+            x = int(input("Type 0 for Rock, 1 for Paper, or 2 for Scissors: "))
+            if x not in [0, 1, 2]:
+                print("Invalid choice. Exiting the game. Thanks for playing!")
+                break
+
+            print(f"\nYou picked:\n{weapons[x]}")
+
+            # Computer's choice
+            c = random.randint(0, 2)
+            print(f"The computer picked:\n{weapons[c]}")
+
+            # Determine the result
+            if x == c:
+                print("It's a tie!\n")
+            elif (x == 0 and c == 2) or (x == 1 and c == 0) or (x == 2 and c == 1):
+                print("You win!\n")
+            else:
+                print("You lose!\n")
+
+        except ValueError:
+            print("Invalid input! Please type a number (0, 1, or 2).")
+
+if __name__ == "__main__":
+    play_game()
